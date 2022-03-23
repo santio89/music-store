e4c298ef528d37d8cb894696736be6b94f91b680
santio89 <santiolais1989@gmail.com>
2022-03-23 00:19:23 -0300

    added css framework

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import '../src/styles/App.css';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import 'bulma/css/bulma.min.css';
 
 function App() {
   let hello = "hello world";
   let app = "react app";
+  
+
 
   return (
+    <>
+    
+    <NavBar />
     <div className="App">
       <div className="hello">{hello}</div>
       <div className="react">{app}</div>
     </div>
+    <Footer />
+
+    </>
   );
 }
 
