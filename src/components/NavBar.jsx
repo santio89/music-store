c4e1b0477956290b97c0f7b64d6ba4d5a9594713
santio89 <santiolais1989@gmail.com>
2022-03-23 20:25:23 -0300

    added nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import '../../src/styles/NavBar.css';
 
 export default function NavBar(){
     return(
         <>
         
-        <nav className="navbar is-fixed-top is-danger has-shadow is-spaced" role="navigation" aria-label="main navigation">
+        <nav className="navbar is-fixed-top is-danger has-shadow is-spaced nav" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-            <a className="navbar-item" href="https://bulma.io">
-            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+            <a className="navbar-item is-size-2 nav__logo" href="index.html">
+                MusicStore
             </a>
 
-            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <a role="button" className="navbar-burger is-large" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
@@ -18,46 +19,51 @@ export default function NavBar(){
         </div>
 
         <div id="navbarBasicExample" className="navbar-menu">
-            <div className="navbar-start">
-            <a className="navbar-item">
-                Home
-            </a>
-
-            <a className="navbar-item">
-                Documentation
-            </a>
-
-            <div className="navbar-item has-dropdown is-hoverable">
-                <a className="navbar-link">
-                More
+            <div className="navbar-end is-size-4-widescreen is-size-5-desktop is-size-4-touch">
+                <a className="navbar-item">
+                    Inicio
                 </a>
 
-                <div className="navbar-dropdown">
                 <a className="navbar-item">
-                    About
+                    Nuevos Lanzamientos
                 </a>
+
                 <a className="navbar-item">
-                    Jobs
+                    <i class="bi bi-cart-fill is-size-3-widescreen is-size-4-desktop is-size-3-touch"></i>
                 </a>
-                <a className="navbar-item">
-                    Contact
-                </a>
-                <hr className="navbar-divider"/>
-                <a className="navbar-item">
-                    Report an issue
-                </a>
-                </div>
-            </div>
+
+            {/* MENU DESPLEGABLE, POR EL MOMENTO NO LO USARE
+                <div className="navbar-item has-dropdown is-hoverable">
+                    <a className="navbar-link">
+                    More
+                    </a>
+
+                    <div className="navbar-dropdown">
+                    <a className="navbar-item">
+                        About
+                    </a>
+                    <a className="navbar-item">
+                        Jobs
+                    </a>
+                    <a className="navbar-item">
+                        Contact
+                    </a>
+                    <hr className="navbar-divider"/>
+                    <a className="navbar-item">
+                        Report an issue
+                    </a>
+                    </div>
+                </div> */}
             </div>
 
             <div className="navbar-end">
             <div className="navbar-item">
                 <div className="buttons">
-                <a className="button is-primary">
-                    <strong>Sign up</strong>
+                <a className="button is-dark is-size-5-widescreen is-size-6-desktop is-size-5-touch">
+                    <strong>Crear Cuenta</strong>
                 </a>
-                <a className="button is-light">
-                    Log in
+                <a className="button is-light is-size-5-widescreen is-size-6-desktop is-size-5-touch">
+                    Entrar
                 </a>
                 </div>
             </div>
