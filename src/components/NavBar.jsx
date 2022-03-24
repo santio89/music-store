2b40b37046e8203e310d079d9b36067262ef02dc
santio89 <santiolais1989@gmail.com>
2022-03-24 15:22:26 -0300

    small fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,49 +34,49 @@ export default function NavBar(){
         <>
         
         <nav className="navbar is-fixed-top is-danger has-shadow is-spaced nav" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-            <a className="navbar-item is-size-2 nav__logo" href="index.html">
-                MusicStore
-            </a>
-
-            <a role="button" className="navbar-burger is-large" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            </a>
-        </div>
-
-        <div id="navbarBasicExample" className="navbar-menu">
-            <div className="navbar-end is-size-4-widescreen is-size-5-desktop is-size-4-touch">
-                <a className="navbar-item">
-                    Inicio
+            <div className="navbar-brand">
+                <a className="navbar-item is-size-2 nav__logo" href="index.html">
+                    MusicStore
                 </a>
 
-                <a className="navbar-item searchBar__wrapper">
-                    <div className="searchBar">
-                        <input type="search" placeholder="Buscar" className="searchBar__input"></input>
-                        <i class="bi bi-search searchBar__icon"></i>
-                    </div>
-                </a>
-
-                <a className="navbar-item">
-                    <i class="bi bi-cart-fill is-size-3-widescreen is-size-4-desktop is-size-3-touch"></i>
+                <a role="button" className="navbar-burger is-large" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
                 </a>
             </div>
 
-            <div className="navbar-end">
-            <div className="navbar-item">
-                <div className="buttons">
-                <a className="button is-dark is-size-5-widescreen is-size-6-desktop is-size-5-touch">
-                    <strong>Crear Cuenta</strong>
-                </a>
-                <a className="button is-light is-size-5-widescreen is-size-6-desktop is-size-5-touch">
-                    Entrar
-                </a>
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-end is-size-4-widescreen is-size-5-desktop is-size-4-touch">
+                    <a className="navbar-item">
+                        Inicio
+                    </a>
+
+                    <a className="navbar-item searchBar__wrapper">
+                        <div className="searchBar">
+                            <input type="search" placeholder="Buscar" className="searchBar__input"></input>
+                            <i class="bi bi-search searchBar__icon"></i>
+                        </div>
+                    </a>
+
+                    <a className="navbar-item">
+                        <i class="bi bi-cart-fill is-size-3-widescreen is-size-4-desktop is-size-3-touch"></i>
+                    </a>
+                </div>
+
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <a className="button is-dark is-size-5-widescreen is-size-6-desktop is-size-5-touch">
+                                <strong>Crear Cuenta</strong>
+                            </a>
+                            <a className="button is-light is-size-5-widescreen is-size-6-desktop is-size-5-touch">
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
         </nav>
 
         </>
