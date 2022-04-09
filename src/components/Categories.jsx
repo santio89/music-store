b5c883403dd25051e57b7b7add0ddf7d6800d874
santio89 <santiolais1989@gmail.com>
2022-04-09 00:47:08 -0300

    adding categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -10,9 +10,9 @@ export default function Categories() {
             <Link to="/categories/pop" className='Categories__pop'>POP</Link>
             <Link to="/categories/blues" className='Categories__blues'>BLUES</Link>
             <Link to="/categories/jazz" className='Categories__jazz'>JAZZ</Link>
-            <Link to="/categories/hip+hop" className='Categories__rap'>RAP</Link>
+            <Link to="/categories/hip+hop" className='Categories__rap'>HIP HOP</Link>
             <Link to="/categories/reggae" className='Categories__reggae'>REGGAE</Link>
-            <Link to="/categories/electronic" className='Categories__techno'>TECHNO</Link>
+            <Link to="/categories/electronic" className='Categories__techno'>ELECTRONIC</Link>
             <Link to="/categories/country" className='Categories__country'>COUNTRY</Link>
             <Link to="/categories/classical" className='Categories__classical'>CLASSICAL</Link>
             <Link to="/categories/funk" className='Categories__funk'>FUNK</Link>
