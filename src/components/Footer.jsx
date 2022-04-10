b285e805dc54233bdb86023af517a2d69f36347e
santio89 <santiolais1989@gmail.com>
2022-04-10 00:09:11 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import '../../src/styles/css/footer.css';
+import { Link } from 'react-router-dom'
 
 export default function Footer(){
+
     return(
         <>
-        <footer>
-            <p>
-                footer
-            </p>
+        <footer className="footer">
+            <Link to="/" onClick={()=>window.scrollTo(0,0)}>◖Más Vistos</Link>
+            <Link to="/categories" onClick={()=>window.scrollTo(0,0)}>◖Categorías</Link>
+            <a href="https://santiweb.netlify.app/" className="footer__santi">santiWeb</a>
         </footer>
         </>
     )
