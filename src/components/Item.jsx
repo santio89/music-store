8403b17e1d3024be780eaf0a185402a11effa8dc
santio89 <santiolais1989@gmail.com>
2022-04-11 00:15:58 -0300

    small fix

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,16 +7,17 @@ export default function Item({id, title, img, price}){
      
 
     return(
-        <>
-            <div className="Item">
-                <div className="Item__imgWrapper"><img className="Item__img" src={img} alt={"cover_image_"+id}></img></div>
-                <div className="Item__content">
-                    <p className="Item__content__price">${price}</p>
-                    <h3 className="Item__content__title">{title}</h3>
+        <>  
+            <Link to={`/item/${id}`} className="ItemLink">
+                <div className="Item">
+                    <div className="Item__imgWrapper"><img className="Item__img" src={img} alt={"cover_image_"+id}></img></div>
+                    <div className="Item__content">
+                        <p className="Item__content__price">${price}</p>
+                        <h3 className="Item__content__title">{title}</h3>
+                    </div>
+                    <div className="Item__details" onClick={()=>window.scrollTo(0,0)}>Detalles</div>
                 </div>
-                <Link to={`/item/${id}`} className="Item__details" onClick={()=>window.scrollTo(0,0)}>Detalles</Link>
-            </div>
-            
+            </Link>
         </>
     )
 }
