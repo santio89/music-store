6b6636065db0c0a78a432feb4056e899ec67e59a
santio89 <santiolais1989@gmail.com>
2022-04-16 22:23:11 -0300

    adding more changes

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -18,18 +18,20 @@ export default function Checkout() {
             <div className='Checkout__details'>
               <ul className='Checkout__details__list'>
                 <h3>Lista de compra</h3>
-                <li><span>TITULO</span><span>ARTISTA</span><span>PRECIO</span><span>CANT.</span><span>SUBT.</span></li>
+                {carrito.length===0?null:<li className='Checkout__details__list__header'><span>TITULO</span><span>ARTISTA</span><span>PRECIO</span><span>CANT.</span><span>SUBT.</span></li>}
+               
                 {
                   carrito.map(item=>
                     <li key={item.id}><span>{item.item.title}</span> <span>{item.item.artists_sort}</span><span>${item.item.precio}</span><span>1</span><span>$subt</span></li>
                   )
                 }
-                <li className='Checkout__details__list__total'>TOTAL: ${"total"}</li>
+                {carrito.length===0?<li>No hay productos en el carrito</li>:<li className='Checkout__details__list__total'>TOTAL: ${"total"}</li>}
               </ul>
               <div className='Checkout__details__resumen'>
                 <h3>Resumen</h3>
-                <div className='Checkout__details__resumen__reset'>
+                <div className='Checkout__details__resumen__buttons'>
                   <button onClick={()=>{clear()}}><i className="bi bi-cart-x-fill"></i>&nbsp;VACIAR CARRITO</button>
+                  <button ><i class="bi bi-cart-check-fill"></i>&nbsp;FINALIZAR COMPRA</button>
                 </div>
               </div>
             </div>
