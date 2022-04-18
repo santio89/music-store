0a4f179f419f41e71a69ebd1b4773a1bd1a56770
santio89 <santiolais1989@gmail.com>
2022-04-17 23:56:29 -0300

    using context

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../Context/CartContext';
 import { useNavigate } from 'react-router-dom';
@@ -8,6 +8,11 @@ export default function Checkout() {
   const history = useNavigate();
 
   const {carrito, cartClear, cartRemove} = useContext(CartContext);
+  const [total, setTotal] = useState(0);
+
+  useEffect(()=>{
+    setTotal(carrito.reduce((total, item)=>total+=item?.item?.precio * item?.item?.count, 0));
+  }, [carrito])
   
   return (  
     <div className="CheckoutWrapper">
@@ -26,11 +31,15 @@ export default function Checkout() {
                     }
                   )
                 }
-                {carrito.length===0?<li>No hay productos en el carrito</li>:<li>TOTAL: ${carrito.reduce((total, item)=>total+=item?.item?.precio * item?.item?.count, 0)}</li>}
+                {carrito.length===0?<li>No hay productos en el carrito</li>:<li>TOTAL: ${total}</li>}
               </ul>
               <div className='Checkout__details__resumen'>
                 <h3>Resumen</h3>
                 <div className="Checkout__details__resumen__info">
+                  <div className="Checkout__details__resumen__text">
+                    <p>Items en carrito: </p>
+                    <p>Total: </p>
+                  </div>
                   <div className='Checkout__details__resumen__buttons'>
                     <button onClick={()=>{cartClear()}}><i className="bi bi-cart-x-fill"></i>&nbsp;VACIAR CARRITO</button>
                     <Link to="/" ><i className="bi bi-cart-plus-fill"></i>&nbsp;SEGUIR COMPRANDO</Link>
