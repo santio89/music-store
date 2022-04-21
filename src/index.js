d37b0e57c7d86e061d0b79790500b9a9fe5ecd08
santio89 <santiolais1989@gmail.com>
2022-04-21 20:42:23 -0300

    installing firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,29 @@ import '../src/styles/css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArMzAtSSMvG-uPS9TNZmQdbZJ7xE0rVAk",
+  authDomain: "music-store-54f43.firebaseapp.com",
+  projectId: "music-store-54f43",
+  storageBucket: "music-store-54f43.appspot.com",
+  messagingSenderId: "545067547241",
+  appId: "1:545067547241:web:25a44c4267ba859a8780f8",
+  measurementId: "G-QE17DTKS4Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
