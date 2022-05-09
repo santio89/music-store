12e48104cf984f1a6d4176f710534f7bb4e13765
santio89 <santiolais1989@gmail.com>
2022-05-09 17:31:22 -0300

    small fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,7 +15,7 @@ export default function SearchBar({ navClosed }) {
             if (search) {
                 navClosed();
                 window.scrollTo(0, 0);
-                e.target.value = "";
+                setSearch("");
                 navigate("/search/" + search);
             } else {
                 return;
@@ -27,11 +27,12 @@ export default function SearchBar({ navClosed }) {
     return (
         <div className="navbar-item searchBar__wrapper">
             <div className="searchBar">
-                <input type="text" placeholder="Buscar" className="searchBar__input" onKeyUp={handleSearch}></input>
+                <input type="text" placeholder="Buscar" className="searchBar__input" value={search} onChange={(e)=>{setSearch(e.currentTarget.value)}} onKeyUp={handleSearch}></input>
                 <Link to={"/search/" + search} onClick={e =>{ 
                     if(search){
                         window.scrollTo(0, 0); 
-                        navClosed()
+                        navClosed();
+                        setSearch("");
                     } else{
                         e.preventDefault()
                     }}} className="searchBar__icon"><i className="bi bi-search"></i></Link>
