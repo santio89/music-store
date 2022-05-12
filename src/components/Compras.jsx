d875ee8c12061c50dcfba935e77332958626c0fb
santio89 <santiolais1989@gmail.com>
2022-05-12 20:22:16 -0300

    small fix

--- src/components/Compras.jsx
+++ src/components/Compras.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import '../styles/css/Compras.css'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function Compras() {
   return (
     <div className='ComprasWrapper'>
-        <div className='Compras'>
-            <h1 className='Compras__title'>Mis Compras</h1>
-        </div>
+      <AnimatePresence>
+        <motion.div className='Compras' key={"Compras"} initial={{ opacity: 0, x: "-120%" }}
+          animate={{ opacity: 1, x: "0%" }}
+          exit={{ opacity: 0, x: "120%" }}
+          transition={{ type: 'tween', duration: .4, ease: "easeInOut" }}>
+          <h1 className='Compras__title'>Mis Compras</h1>
+        </motion.div>
+      </AnimatePresence>
     </div>
   )
 }
